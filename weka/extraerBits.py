0e6c26fed1db603b704114c52aabb3209184b2e7
irvynX <irvyn.xicale@alumno.buap.mx>
2023-02-22 18:16:37 -0600

    sdf

--- weka/extraerBits.py
+++ weka/extraerBits.py
@@ -1,17 +1,13 @@
-#librerias
-import numpy as np
-import cv2 as cv
+from PIL import Image
 
-#cargar imagenes
-img = "1.1.png"
+# Abre la imagen
+img = Image.open("imagen.jpg")
 
-#leer imagen
-imgColor = cv.imread(img,1)
-imgColorB = cv.imread(img,1)
-imgColorG = cv.imread(img,1)
-imgColorR = cv.imread(img,1)
-imgColorN = cv.imread(img,1)
+# Obtiene la anchura y altura de la imagen
+width, height = img.size
 
-#mostrar imagen
-cv.imshow("imgColor",imgColorB)
-print("hola mundo")
+# Itera sobre cada pixel y extrae el valor RGB
+for x in range(width):
+    for y in range(height):
+        r, g, b = img.getpixel((x,y))
+        print("Pixel en x=", x, " y=", y, " R=", r, " G=", g, " B=", b)
