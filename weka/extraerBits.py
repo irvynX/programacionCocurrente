2c600151dcdfe937ce2945b29d260b4ae05893b9
irvynX <irvyn.xicale@alumno.buap.mx>
2023-02-22 18:24:16 -0600

    ds

--- weka/extraerBits.py
+++ weka/extraerBits.py
@@ -1,13 +1,23 @@
 from PIL import Image
 
 # Abre la imagen
-img = Image.open("imagen.jpg")
+img = Image.open("imagen.png")
 
 # Obtiene la anchura y altura de la imagen
 width, height = img.size
 
+# Verifica si la imagen tiene un canal alfa
+if img.mode in ("RGBA", "LA") or (img.mode == "P" and "transparency" in img.info):
+    alpha_channel = True
+else:
+    alpha_channel = False
+
 # Itera sobre cada pixel y extrae el valor RGB
 for x in range(width):
     for y in range(height):
-        r, g, b = img.getpixel((x,y))
-        print("Pixel en x=", x, " y=", y, " R=", r, " G=", g, " B=", b)
+        if alpha_channel:
+            r, g, b, a = img.getpixel((x,y))
+            print("Pixel en x=", x, " y=", y, " R=", r, " G=", g, " B=", b, " A=", a)
+        else:
+            r, g, b = img.getpixel((x,y))
+            print("Pixel en x=", x, " y=", y, " R=", r, " G=", g, " B=", b)
