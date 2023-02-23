2703e5eb2ab9107716af5783fc2828aa90800233
irvynX <irvyn.xicale@alumno.buap.mx>
2023-02-23 15:25:06 -0600

    asd

--- weka/extraerBits.py
+++ weka/extraerBits.py
@@ -1,7 +1,8 @@
 from PIL import Image
 
 # Abre la imagen y conviértela en modo "RGB"
-img = Image.open("1.3.png").convert("RGB")
+#img = Image.open("1.3.png").convert("RGB")
+img = Image.open("imgP/m2.png").convert("RGB")
 imgOut = Image.new('RGB',(1500,1500),color='black')
 
 # Obtiene el ancho y el alto de la imagen
@@ -16,9 +17,10 @@ for y in range(height):
     for x in range(width):
         r, g, b = img.getpixel((x, y))
         red_values.append(r)
-        imgOut.putpixel((x, y),(red_values[i], 0, 0))
+        print(x,y,r,g,b)
+        imgOut.putpixel((x, y),(r, g, b))
         i = i + 1
 
 # Imprime los valores de rojo de los primeros 10 píxeles
-print(red_values[:10])
+#print(red_values[:10])
 imgOut.save("salida1.png")
