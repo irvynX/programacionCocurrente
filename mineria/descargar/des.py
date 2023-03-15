9566198bc3ebe0e6ee4c006d645b4fc087d9e9e3
Jesus Pichon <78553293+JesusPichon@users.noreply.github.com>
2023-03-14 23:14:50 -0600

    Delete mineria directory
    Eliminacion del directorio

--- mineria/descargar/des.py
+++
@@ -1,9 +0,0 @@
-from google_images_download import google_images_download
-
-response = google_images_download.googleimagesdownload()
-
-arguments = {
-    "keywords": "gato",
-    "limit": 10,
-    "print_urls": True
-}
